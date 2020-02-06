114b6990bdc8e840444a70a9b2c0296ab6b46005
Rodney <rodney@boonlogic.com>
2020-02-06 16:24:02 -0600

    tee output

--- unit-test.sh
+++ unit-test.sh
@@ -4,7 +4,7 @@
 # sudo ln -s /path/to/bin/matlab /usr/local/bin/matlab
 
 # Update matlab path, Go to Tests/ folder, Run unit test. 
-matlab -nodisplay -nosplash -nodesktop -batch 'addpath(pwd);cd("Tests");run(BoonNanoSDKTest)'
+matlab -nodisplay -nosplash -nodesktop -batch 'addpath(pwd);cd("Tests");run(BoonNanoSDKTest)' | tee test.log
 
 # Grep matlab log for results eg: '23 Passed, x Failed, 0 Incomplete.'
 errorcount=$(cat test.log | grep -o -P '(?<=Passed, ).*(?= Failed,)')
